cf7c7e1e2981d16af55ed19d547690ade15be478
gena <gennadiy.donchyts@gmail.com>
2015-03-31 10:39:03 +0200

    add missing files

---
+++ src/BasicModelInterface/BasicModelInterfaceExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicModelInterface
+{
+    public static class BasicModelInterfaceExtensions
+    {
+        public static T GetValue<T>(this IBasicModelInterface model, string variable)
+        {
+            var values = model.GetValues(variable);
+            return (T)values.GetValue(0);
+        }
+
+        public static T GetValue<T>(this IBasicModelInterface model, string variable, int index)
+        {
+            var values = model.GetValues(variable);
+            return (T)values.GetValue(index);
+        }
+
+        public static void SetValue<T>(this IBasicModelInterface model, string variable, T value)
+        {
+            var length = BasicModelInterfaceLibrary.GetTotalLength(model.GetShape(variable));
+            var values = Enumerable.Repeat(value, length).ToArray();
+
+            model.SetValues(variable, values);
+        }
+
+        public static void SetValue<T>(this IBasicModelInterface model, string variable, int index, T value)
+        {
+            var shape = model.GetShape(variable);
+
+            if (shape.Length != 1)
+            {
+                throw new NotSupportedException("use SetValue(variable, start, count, values)");
+            }
+
+            //model.SetValues(variable, new int[] { index }, new int[] { 1 }, new T[] { value });
+            model.SetValues(variable, new int[] { index },  new T[] { value });
+        }
+    }
+}
