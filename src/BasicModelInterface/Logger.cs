9b83dcfa138efebcebbef378b818de2525c0e03a
hrajagers <bert.jagers@deltares.nl>
2017-09-08 11:03:06 +0200

    Updating log levels to make them consistent across BMI language interfaces and message handling

--- src/BasicModelInterface/Logger.cs
+++ src/BasicModelInterface/Logger.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 namespace BasicModelInterface
 {
-    public enum Level { All, Debug, Info, Warning, Error, Fatal, Off };
+    public enum Level { Level_All, Level_Debug, Level_Info, Level_Warning, Level_Error, Level_Fatal, Level_None };
 
     public delegate void Logger([In] Level level, [In][MarshalAs(UnmanagedType.LPStr)] string message);
 }
